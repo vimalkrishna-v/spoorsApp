f8051d1fe3e8d2e9b49d8786869326995caa57b6
Niya Shiyas <niya.s@redbus.com>
2025-07-09 15:01:06 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const database = require('./config/database');
 const { errorHandler, notFoundHandler } = require('./middleware/errorHandler');
 
